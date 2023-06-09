a148063b6c1398af20ca14af6039031939d2fd69
James Ross <git@james-ross.co.uk>
2023-06-09 13:14:54 +0100

    feat: Add network I/O to system agent
    Closes #8

--- Services/Agents/System.cs
+++ Services/Agents/System.cs
@@ -11,6 +11,7 @@ namespace HA_Agent.Agents
         readonly PerformanceCounter? CPUPerformance;
 
         readonly Dictionary<string, WindowsDiskIO>? WindowsDiskIO;
+        readonly Dictionary<string, WindowsNetworkIO>? WindowsNetworkIO;
 
         public System(HomeAssistant homeAssistant, IConfigurationSection config, bool verbose, bool dryRun)
             : base(homeAssistant, Environment.MachineName, config["name"] ?? Environment.MachineName, verbose, dryRun)
@@ -18,6 +19,7 @@ namespace HA_Agent.Agents
             CPUPerformance = OperatingSystem.IsWindows() ? new PerformanceCounter("Processor Information", "% Processor Performance", "_Total") : null;
             CPUUtility = OperatingSystem.IsWindows() ? new PerformanceCounter("Processor Information", "% Processor Utility", "_Total") : null;
             WindowsDiskIO = OperatingSystem.IsWindows() ? new() : null;
+            WindowsNetworkIO = OperatingSystem.IsWindows() ? new() : null;
             UpdateCounterLists();
         }
 
@@ -28,6 +30,7 @@ namespace HA_Agent.Agents
                 CPUPerformance?.NextValue();
                 CPUUtility?.NextValue();
                 if (WindowsDiskIO != null) foreach (var diskIo in WindowsDiskIO) diskIo.Value.NextValue();
+                if (WindowsNetworkIO != null) foreach (var networkIo in WindowsNetworkIO) networkIo.Value.NextValue();
                 UpdateCounterLists();
             }
             return Task.CompletedTask;
@@ -59,6 +62,12 @@ namespace HA_Agent.Agents
                 await PublishSensor("sensor", $"Disk {data.Name} read bytes/sec", icon: "mdi:folder-upload", stateClass: "measurement", unitOfMeasurement: "MiB/s", entityCategory: "diagnostic", state: data.ReadMiBPerSec.ToString("F1"));
                 await PublishSensor("sensor", $"Disk {data.Name} write bytes/sec", icon: "mdi:folder-download", stateClass: "measurement", unitOfMeasurement: "MiB/s", entityCategory: "diagnostic", state: data.WriteMiBPerSec.ToString("F1"));
             }
+            foreach (var data in GetNetworkIO())
+            {
+                await PublishSensor("sensor", $"Network {data.Name} bytes/sec", icon: "mdi:network", stateClass: "measurement", unitOfMeasurement: "KiB/s", entityCategory: "diagnostic", state: data.TotalKiBPerSec.ToString("F1"));
+                await PublishSensor("sensor", $"Network {data.Name} received bytes/sec", icon: "mdi:download-network", stateClass: "measurement", unitOfMeasurement: "KiB/s", entityCategory: "diagnostic", state: data.ReadKiBPerSec.ToString("F1"));
+                await PublishSensor("sensor", $"Network {data.Name} sent bytes/sec", icon: "mdi:upload-network", stateClass: "measurement", unitOfMeasurement: "KiB/s", entityCategory: "diagnostic", state: data.WriteKiBPerSec.ToString("F1"));
+            }
             if (OperatingSystem.IsWindows() && CPUPerformance != null && CPUUtility != null)
             {
                 var icon = Environment.Is64BitOperatingSystem ? "mdi:cpu-64-bit" : "mdi:cpu-32-bit";
@@ -77,30 +86,56 @@ namespace HA_Agent.Agents
 
         void UpdateCounterLists()
         {
-            if (OperatingSystem.IsWindows() && WindowsDiskIO != null)
+            if (OperatingSystem.IsWindows())
             {
                 VerboseLog("UpdateCounterLists: Start");
 
-                var query = new ObjectQuery("SELECT * FROM Win32_PerfFormattedData_PerfDisk_LogicalDisk");
-                var searcher = new ManagementObjectSearcher(query);
-                var collection = searcher.Get();
-                var names = new HashSet<string>();
-                foreach (var disk in collection)
+                if (WindowsDiskIO != null)
                 {
-                    var name = (string)disk["Name"];
-                    // Only accept two-letter drive names (e.g. allow "C:", skip "HarddiskVolume1" and "_Total")
-                    if (name.Length != 2) continue;
-                    names.Add(name);
-                    if (!WindowsDiskIO.ContainsKey(name))
+                    var query = new ObjectQuery("SELECT * FROM Win32_PerfFormattedData_PerfDisk_LogicalDisk");
+                    var searcher = new ManagementObjectSearcher(query);
+                    var collection = searcher.Get();
+                    var names = new HashSet<string>();
+                    foreach (var disk in collection)
                     {
-                        VerboseLog($"UpdateCounterLists: Add Disk: {name}");
-                        WindowsDiskIO.Add(name, new WindowsDiskIO(name));
+                        var name = (string)disk["Name"];
+                        // Only accept two-letter drive names (e.g. allow "C:", skip "HarddiskVolume1" and "_Total")
+                        if (name.Length != 2) continue;
+                        names.Add(name);
+                        if (!WindowsDiskIO.ContainsKey(name))
+                        {
+                            VerboseLog($"UpdateCounterLists: Add Disk: {name}");
+                            WindowsDiskIO.Add(name, new WindowsDiskIO(name));
+                        }
+                    }
+                    foreach (var name in WindowsDiskIO.Keys.Where(name => !names.Contains(name)).ToList())
+                    {
+                        VerboseLog($"UpdateCounterLists: Remove Disk: {name}");
+                        WindowsDiskIO.Remove(name);
                     }
                 }
-                foreach (var name in WindowsDiskIO.Keys.Where(name => !names.Contains(name)).ToList())
+
+                if (WindowsNetworkIO != null)
                 {
-                    VerboseLog($"UpdateCounterLists: Remove Disk: {name}");
-                    WindowsDiskIO.Remove(name);
+                    var query = new ObjectQuery("SELECT * FROM Win32_PerfFormattedData_Tcpip_NetworkInterface");
+                    var searcher = new ManagementObjectSearcher(query);
+                    var collection = searcher.Get();
+                    var names = new HashSet<string>();
+                    foreach (var network in collection)
+                    {
+                        var name = (string)network["Name"];
+                        names.Add(name);
+                        if (!WindowsNetworkIO.ContainsKey(name))
+                        {
+                            VerboseLog($"UpdateCounterLists: Add Network: {name}");
+                            WindowsNetworkIO.Add(name, new WindowsNetworkIO(name));
+                        }
+                    }
+                    foreach (var name in WindowsNetworkIO.Keys.Where(name => !names.Contains(name)).ToList())
+                    {
+                        VerboseLog($"UpdateCounterLists: Remove Network: {name}");
+                        WindowsNetworkIO.Remove(name);
+                    }
                 }
 
                 VerboseLog("UpdateCounterLists: Finish");
@@ -208,6 +243,19 @@ namespace HA_Agent.Agents
                 foreach (var disk in WindowsDiskIO)
                 {
                     list.Add(new NameValueIO(disk.Key, disk.Value.BytesReadPerSecond.NextValue(), disk.Value.BytesWritePerSecond.NextValue()));
+                }
+            }
+            return list;
+        }
+
+        List<NameValueIO> GetNetworkIO()
+        {
+            var list = new List<NameValueIO>();
+            if (OperatingSystem.IsWindows() && WindowsNetworkIO != null)
+            {
+                foreach (var network in WindowsNetworkIO)
+                {
+                    list.Add(new NameValueIO(network.Key, network.Value.BytesReceivedPerSecond.NextValue(), network.Value.BytesSentPerSecond.NextValue()));
                 }
             }
             return list;
---
+++ Services/WindowsNetworkIO.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+using System.Runtime.Versioning;
+
+namespace HA_Agent.Services
+{
+    [SupportedOSPlatform("Windows")]
+    record WindowsNetworkIO(string AdapterName)
+    {
+        public PerformanceCounter BytesReceivedPerSecond { get; } = new PerformanceCounter("Network Adapter", "Bytes Received/sec", AdapterName);
+        public PerformanceCounter BytesSentPerSecond { get; } = new PerformanceCounter("Network Adapter", "Bytes Sent/sec", AdapterName);
+
+        public void NextValue()
+        {
+            BytesReceivedPerSecond.NextValue();
+            BytesSentPerSecond.NextValue();
+        }
+    }
+}
